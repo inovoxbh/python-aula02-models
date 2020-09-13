a4d9236468c2862d049537a19242012c01a784be
Bruno G Alcantara <inovox@gmail.com>
2020-09-13 16:57:07 -0300

    entrega exercicio 02

--- aplicacao/models.py
+++ aplicacao/models.py
@@ -4,6 +4,9 @@ class Departamento(models.Model):
     sigla = models.CharField(max_length=6)
     descricao = models.CharField(max_length=30)
 
+    def __str__ (self):
+        return self.sigla + ' ' + self.descricao
+
 class PessoaManager(models.Manager):
     def create_pessoa(self, nome, sexo):
         pessoa = self.create(nome=nome, sexo=sexo)
@@ -11,6 +14,31 @@ class PessoaManager(models.Manager):
 
     def print_pessoa(self):
         print(self.none,self.sexo)
+
+    # querie para retornar todas as pessoas
+    def todos(self):
+        result = self.all()
+        return result
+
+    # querie para retornar pessoas do sexto masculino
+    def homens(self):
+        result = self.filter(sexo="M")
+        return result
+
+    # querie para retornar pessoas do sexto feminino
+    def mulheres(self):
+        result = self.filter(sexo="F")
+        return result
+
+    # querie para retornar pessoas com idade igual ou superior a 18 anos
+    def adultos(self):
+        result = self.filter(idade__gte=18)
+        return result
+
+    # querie para retornar pessoas com idade inferior a 18 anos
+    def menores(self):
+        result = self.filter(idade__lt=18)
+        return result
 
 class Pessoa(models.Model):
     nome = models.CharField(max_length=30)
@@ -30,7 +58,8 @@ class Pessoa(models.Model):
         Departamento,
         on_delete=models.RESTRICT,
         null=True,
-        related_name='chefia_dpto'
+        related_name='chefia_dpto',
+        blank=True
     )
 
     OPCOES_SEXO = [
@@ -44,5 +73,8 @@ class Pessoa(models.Model):
         choices=OPCOES_SEXO,
         default='I'
     )
+
+    def __str__ (self):
+        return self.nome + ' ' + self.sobrenome
 
     objects = PessoaManager()
